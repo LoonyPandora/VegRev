e269747d346bc1e7740f8e22a94a46c31583c6d4
LoonyPandora <loonypandora@gmail.com>
2010-04-17 18:02:38 +0100

    working on priv messages

--- lib/VR/Model/Message.pm
+++ lib/VR/Model/Message.pm
@@ -16,39 +16,56 @@ use vars '@EXPORT_OK';
 @EXPORT_OK = qw();
 
 
+sub load_inbox {
+  my ($user_id, $offset, $limit) = @_;
 
-sub load_conversations {
-  my ($offset, undef) = @_;
-
-	my $query = qq{
-SELECT pm_threads.pm_thread_id, pm_threads.pm_subject, pm_threads.pm_last_msg_time, pm_threads.pm_total_messages, pm_threads.pm_sender_unread, pm_threads.pm_receiver_unread, pm_threads.pm_receiver_delete, pm_threads.pm_sender_delete, pm_threads.sender.user_id AS sender_user_id, sender.display_name AS sender_display_name, sender.avatar AS sender_avatar, sender.user_name AS sender_user_name, send_spec_groups.spec_group_color AS sender_spec_group_color, receiver.user_id AS receiver_user_id, receiver.display_name AS receiver_display_name, receiver.avatar AS receiver_avatar, receiver.user_name AS receiver_user_name, rec_spec_groups.spec_group_color AS receiver_spec_group_color, last.user_id AS last_user_id, last.user_name AS last_user_name, last.display_name AS last_display_name, last_spec_groups.spec_group_color AS last_spec_group_color,
-CASE
-WHEN pm_sender_id = ? AND pm_sender_delete != '1' THEN '1'
-END AS sender_visible,
-CASE
-WHEN pm_receiver_id = ? AND pm_receiver_delete != '1' THEN '1'
-END AS receiver_visible,
-CASE
-WHEN pm_receiver_id = pm_sender_id THEN '1'
-END AS self_message
-FROM pm_threads
-INNER JOIN users as sender ON sender.user_id = pm_threads.pm_sender_id
-INNER JOIN users as receiver ON receiver.user_id = pm_threads.pm_receiver_id
-INNER JOIN users as last ON last.user_id = pm_threads.pm_last_poster_id
-LEFT JOIN special_groups AS send_spec_groups ON sender.spec_group_id = send_spec_groups.spec_group_id
-LEFT JOIN special_groups AS rec_spec_groups ON receiver.spec_group_id = rec_spec_groups.spec_group_id
-LEFT JOIN special_groups AS last_spec_groups ON last.spec_group_id = last_spec_groups.spec_group_id
-WHERE (pm_threads.pm_receiver_id = ? AND receiver_visible = '1')
-OR (pm_threads.pm_sender_id = ? AND sender_visible = '1')
-ORDER BY pm_threads.pm_last_msg_time DESC
+	my $sql = q|
+SELECT priv_conversations.thread_id, pm_subject, pm_last_msg_time, pm_last_poster_id,
+sender.user_name AS sender_user_name, sender.display_name AS sender_display_name, sender_user_groups.group_color AS sender_group_color,
+last.user_name AS last_user_name, last.display_name AS last_display_name, last_user_groups.group_color AS last_group_color
+FROM priv_conversations
+LEFT JOIN priv_threads AS priv_threads ON priv_threads.pm_thread_id = priv_conversations.thread_id
+LEFT JOIN priv_conversations AS sender_thread ON priv_conversations.thread_id = sender_thread.thread_id AND sender_thread.user_id != ?
+LEFT JOIN users AS sender ON sender_thread.user_id = sender.user_id
+LEFT JOIN user_groups AS sender_user_groups ON (CASE WHEN sender.group_id = 0 OR sender.group_id IS NULL THEN (SELECT group_id FROM user_groups WHERE sender_user_groups.posts_required <= sender.user_post_num ORDER BY sender_user_groups.posts_required DESC LIMIT 1) ELSE sender.group_id END) = sender_user_groups.group_id
+LEFT JOIN users AS last ON priv_threads.pm_last_poster_id = last.user_id
+LEFT JOIN user_groups AS last_user_groups ON (CASE WHEN last.group_id = 0 OR last.group_id IS NULL THEN (SELECT group_id FROM user_groups WHERE last_user_groups.posts_required <= last.user_post_num ORDER BY last_user_groups.posts_required DESC LIMIT 1) ELSE last.group_id END) = last_user_groups.group_id
+WHERE priv_conversations.user_id = ?
+AND priv_conversations.deleted != '1'
+ORDER BY priv_threads.pm_last_msg_time DESC
 LIMIT ?, ?
-};
+|;
+ 
+  my @bind = (
+    $user_id,
+    $user_id,
+    $offset,
+    $limit,
+  );
 
-	# Execute all queries, and bind to template variables.
-	$vr::loop = $vr::dbh->prepare($query);
-	$vr::loop->execute($vr::viewer{'user_id'}, $vr::viewer{'user_id'}, $vr::viewer{'user_id'}, $vr::viewer{'user_id'}, $offset, $vr::config{'pms_per_page'});
-	$vr::loop->bind_columns(\(@vr::loop{@{$vr::loop->{NAME_lc}}}));  
 
+	my $static_sql = q|
+SELECT COUNT(*) AS total_messages
+FROM priv_conversations
+WHERE priv_conversations.user_id = ?
+AND priv_conversations.deleted != '1'
+LIMIT 1
+|;
+
+  my @static_bind = (
+    $user_id,
+  );
+
+  &VR::Util::read_db(\$sql, \@bind, \$VR::sth{'priv_messages'}, \$VR::db->{'priv_messages'}); 
+  &VR::Util::fetch_db(\$static_sql, \@static_bind, \$VR::db->{'private_total'});
+}
+
+sub write_new_message {
+  
+}
+
+sub reply_to_thread {
+  
 }
 
 
--- lib/VR/Route/Message.pm
+++ lib/VR/Route/Message.pm
@@ -6,10 +6,22 @@
 # Author:       James Aitken <http://www.loonypandora.com/>
 ###############################################################################
 
-get '/messages' => sub {
+
+get r('/messages/?(\d{1,10})?/?') => sub {
+  my ($page) = splat;
+  my $offset = ($page*15) - 15;
+  if ($offset < 0 ) { $offset = 0; $page = 1; }
+
+  if ($VR::viewer{'is_guest'}) { redirect('/'); }
   
-  
-#  &VR::Model::Forum::load_threads($board, '0', '15');
+  &VR::Model::Message::load_inbox($VR::viewer{'user_id'}, $offset, '15');
+
+  %VR::tmpl = (
+    current_route => "/messages",
+    current_page  => $page,
+    total_pages   => int(($VR::db->{'private_total'}{'total_messages'}/15)+0.9999),
+    page_title    => 'inbox',
+  );
 
   template 'inbox';
 };
@@ -24,7 +36,7 @@ get '/messages/sent' => sub {
 };
 
 
-get r('/messages/(\d{1,10})/?') => sub {  
+get r('/messages/inbox/(\d{1,10})/?') => sub {  
   my ($pm_thread) = splat;
   
 #  &VR::Model::Forum::load_threads($board, '0', '15');
