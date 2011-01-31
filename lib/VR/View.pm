357e63f07473a760c953cf018857eb9feaa568c8
James Aitken <loonypandora@gmail.com>
2011-01-31 05:45:04 +0000

    Adding avatar helper

--- lib/VR/View.pm
+++ lib/VR/View.pm
@@ -3,6 +3,7 @@ package VR::View;
 use common::sense;
 use Dancer ':syntax';
 
+use HTML::Entities;
 # This provides view helpers. Named like this so it's the right namespace
 
 
@@ -15,5 +16,22 @@ sub config {
     return $settings->{config}{$setting};
 }
 
+
+# Generates an img tag when passed an avatar, will pass default avatar if there is none.
+sub avatar_img {
+    my ($avatar, $usertext) = @_;
+
+    my $base_url = 'http://www.vegetablerevolution.co.uk/uploads';
+    
+    $avatar     = encode_entities($avatar);
+    $usertext   = encode_entities($usertext);
+
+    if ($avatar && $avatar =~ /^\d+\.\w{3,4}/) {
+        return qq{<img src="$base_url/$avatar" alt="$usertext" />};
+    } else {
+        return qq{<img src="http://vegrev.local/img/placeholder.png" alt="No Avatar" />};
+    }
+    
+}
 
 1;
