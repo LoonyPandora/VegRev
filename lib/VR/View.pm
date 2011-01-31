b90d509873a6a6102212252f3e03aafe82ad5037
James Aitken <loonypandora@gmail.com>
2011-01-31 08:13:29 +0000

    Adding Time Helpers

--- lib/VR/View.pm
+++ lib/VR/View.pm
@@ -4,6 +4,9 @@ use common::sense;
 use Dancer ':syntax';
 
 use HTML::Entities;
+use Time::Duration;
+use Time::Local;
+
 # This provides view helpers. Named like this so it's the right namespace
 
 
@@ -23,6 +26,8 @@ sub avatar_img {
 
     my $base_url = 'http://www.vegetablerevolution.co.uk/uploads';
     
+    # We need to encode the individual items, as this function returns HTML
+    # And so isn't encoded automagically in the template
     $avatar     = encode_entities($avatar);
     $usertext   = encode_entities($usertext);
 
@@ -31,7 +36,20 @@ sub avatar_img {
     } else {
         return qq{<img src="http://vegrev.local/img/placeholder.png" alt="No Avatar" />};
     }
-    
+
+}
+
+
+sub time_ago {
+    my ($timestamp, $precision) = @_;
+
+    my $seconds = (time - $timestamp);
+
+    if ($seconds < 60) {
+        return 'just now';
+    } else {
+        return ago($seconds, $precision);
+    }
 }
 
 1;
