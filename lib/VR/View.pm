a9ecdb99d82d307c7d73bb4ce92ac166029f4f16
James Aitken <loonypandora@gmail.com>
2011-01-31 02:48:35 +0000

    adding config helper

--- lib/VR/View.pm
+++ lib/VR/View.pm
@@ -1,8 +1,19 @@
 package VR::View;
 
 use common::sense;
+use Dancer ':syntax';
 
 # This provides view helpers. Named like this so it's the right namespace
+
+
+
+# Access app config in templates
+sub config {
+    my $setting = shift;    
+    my $settings = Dancer::Config->settings;
+
+    return $settings->{config}{$setting};
+}
 
 
 1;
