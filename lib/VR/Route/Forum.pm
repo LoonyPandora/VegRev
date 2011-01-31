8145ddd5c03b0c66baf8679d8e69385d4a46b923
James Aitken <loonypandora@gmail.com>
2011-01-31 08:13:59 +0000

    Fixing up forum main view

--- lib/VR/Route/Forum.pm
+++ lib/VR/Route/Forum.pm
@@ -7,16 +7,16 @@ use Dancer::Plugin::Database;
 prefix '/forum';
 
 
-
+# There is always a page variable passed to this, as it is a forwarded route from '/'
 get qr{/(\d+)/?$} => sub {
     my ($page) = splat;
 
-    my $threads_per_page = 30;
-    my $offset = ($threads_per_page * $page) - $threads_per_page;
+    my $per_page  = 30;
+    my $offset    = ($per_page * $page) - $per_page;
 
     my $sth = database->prepare(
         q{
-            SELECT thread.id, subject, url_slug, last_updated, user_name, display_name, avatar, usertext,
+            SELECT thread.id, subject, url_slug, UNIX_TIMESTAMP(last_updated) AS last_updated, user_name, display_name, avatar, usertext,
                 (SELECT count(*) FROM message WHERE message.thread_id = thread.id) AS replies
             FROM thread
             LEFT JOIN user ON latest_post_user_id = user.id
@@ -25,11 +25,11 @@ get qr{/(\d+)/?$} => sub {
         }
     );
 
-    $sth->execute($offset, $threads_per_page);
-    my $recent_threads = $sth->fetchall_arrayref({});
+    $sth->execute($offset, $per_page);
 
     template 'forum', {
-        recent_threads => $recent_threads,
+        page_title      => 'The Forum',
+        recent_threads  => $sth->fetchall_arrayref({}),
     };
 };
 
